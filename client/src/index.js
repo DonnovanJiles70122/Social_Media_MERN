90f10a010d085838d84b03258da0e05273eb6dfb
Donnovan Jiles <djiles@mail.sfsu.edu>
2023-05-19 09:56:19 -0700

    light and dark mode

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import authReducer from "./state";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import { Storage } from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/es/integration/react";
+import persistReducer from "redux-persist/es/persistReducer";
+import { getDefaultService } from "selenium-webdriver/chrome";
+
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore}>
+        <App />
+      </PersistGate>
+    </Provider>
     <App />
   </React.StrictMode>
 );
